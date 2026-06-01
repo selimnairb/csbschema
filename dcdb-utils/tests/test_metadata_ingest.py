dea7b76fbd97a35d0ebaa693763e9f3a3f333d96
Brian Miles <selimnairb@gmail.com>
2026-06-01 10:14:06 -0400

    dcdb-utils: Update unit tests to match new ingest behaviors

--- dcdb-utils/tests/test_metadata_ingest.py
+++ dcdb-utils/tests/test_metadata_ingest.py
@@ -35,36 +35,31 @@ def test_write_vessel_metadata_to_db(data_path, temp_path):
         all_entries = get_metadata_entries(db)
         assert len(all_entries) == 4
 
-        test_entry = all_entries[3]
-        entry_stats: VesselEntrySummaryStats = get_vessel_entry_stats(db, test_entry.key.unique_vessel_id, test_entry.hash)
-        # There's only one entry with this unique ID right now, so the stats are just the values of that entry...
-        # assert stats.min_start_time == test_entry.key.start_time
-        # assert stats.max_start_time == test_entry.key.start_time
-        # assert stats.min_end_time == test_entry.key.end_time
-        # assert stats.max_end_time == test_entry.key.end_time
-
-        # Now ingest a file with the same metadata as an existing entry, but with a later start time.
-        # This should result in a second entry being added to the database.
-        existing_entry = entries[0]
-        doc_path_ex1_newer: Path = data_path / 'example1-newer-start-time'
-        stats = DataIngestStats()
-        write_vessel_metadata_to_db(db, stats, load_vessel_metadata(doc_path_ex1_newer, stats))
-        new_entries = get_metadata_entries(db, unique_vessel_id=unique_vessel_id)
-        assert len(new_entries) == 2
-        new_entry = new_entries[0]
-        assert new_entry.key.start_time == existing_entry.key.start_time
-        assert new_entry.hash == existing_entry.hash
-
         # Now ingest a file with the same metadata as an existing entry, but with an older start time.
-        # This should result in the older start time being added to the db.
+        # This should result in the record being updated to have the older start time.
+        existing_entry = entries[0]
         doc_path_ex1_older: Path = data_path / 'example1-older-start-time'
         stats = DataIngestStats()
         write_vessel_metadata_to_db(db, stats, load_vessel_metadata(doc_path_ex1_older, stats))
         new_entries = get_metadata_entries(db, unique_vessel_id=unique_vessel_id)
         assert len(new_entries) == 1
-        new_entry = new_entries[0]
-        assert new_entry.key.start_time < existing_entry.key.start_time
-        assert new_entry.hash == existing_entry.hash
+        new_entry_st = new_entries[0]
+        assert new_entry_st.key.start_time < existing_entry.key.start_time
+        assert new_entry_st.key.end_time == existing_entry.key.end_time
+        assert new_entry_st.hash == existing_entry.hash
+
+        # Now ingest a file with the same metadata as an existing entry, but with a later end time.
+        # This should result in the record being updated to have the older start time.
+        doc_path_ex1_later: Path = data_path / 'example1-later-end-time'
+        stats = DataIngestStats()
+        write_vessel_metadata_to_db(db, stats, load_vessel_metadata(doc_path_ex1_later, stats))
+        new_entries = get_metadata_entries(db, unique_vessel_id=unique_vessel_id)
+        assert len(new_entries) == 1
+        new_entry_ed = new_entries[0]
+        # Start time was updated with the previous import, so we can't compare to the baseline.
+        assert new_entry_ed.key.start_time == new_entry_st.key.start_time
+        assert new_entry_ed.key.end_time > existing_entry.key.end_time
+        assert new_entry_ed.hash == existing_entry.hash
 
         # Now ingest a file with the same, though rounded, metadata and a newer start time.
         # This should result in no change to the database.
@@ -78,20 +73,17 @@ def test_write_vessel_metadata_to_db(data_path, temp_path):
         assert len(new_entries) == 1
         new_entry = new_entries[0]
         assert new_entry.key.start_time == existing_entry.key.start_time
+        assert new_entry.key.end_time == existing_entry.key.end_time
         assert new_entry.hash == existing_entry.hash
 
         # Finally, test the case where a new metadata entry for an existing vessel is received
-        # that has different metadata, but the same start time -- this should result in the database
-        # not allowing the metadata to be inserted.
+        # that has different metadata, but the same start time and end time
+        # This should result in the database
         doc_path_ex1_diff_hash: Path = data_path / 'example1-same-start-different-hash'
         stats = DataIngestStats()
-        vessel_meta_diff_hash = load_vessel_metadata(doc_path_ex1_diff_hash, stats)
-        exception_thrown = False
-        try:
-            write_vessel_metadata_to_db(db, stats, vessel_meta_diff_hash)
-        except sqlite3.IntegrityError:
-            exception_thrown = True
-        assert exception_thrown
+        write_vessel_metadata_to_db(db, stats, load_vessel_metadata(doc_path_ex1_diff_hash, stats))
+        new_entries = get_metadata_entries(db, unique_vessel_id=unique_vessel_id)
+        assert len(new_entries) == 2
 
     finally:
         db.close()
